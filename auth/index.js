0d9411621a48293354eeae2acdb296ceaedbb81c
Srinath Karthikeyan <sriinathk@gmail.com>
2020-05-01 16:51:34 +0530

    - REDIS PUBSUB - User auth added - User sign up added

--- auth/index.js
+++ auth/index.js
@@ -1,15 +1,32 @@
 const { AuthenticationError } = require('apollo-server');
-const { redis } = require('../redis')
+const { REDIS_ACCESS_KEY } = require('../constants');
+const { getActiveClient } = require('../redis');
 
-const contextMiddleware = token => {
+const authMiddleware = async token => {
     if (!token) {
         throw new AuthenticationError('Please try again by passing Authorization Header')
     } else {
-        return {
-            username: 123,
-            client: redis
+        try {
+            const redis_cli = await getActiveClient()
+            return new Promise((resolve, reject) => redis_cli.client.hexists(REDIS_ACCESS_KEY, token, (err, result) => {
+                if(err) {
+                    reject({reason: 'Something went wrong while authenticating user'})
+                } else if(result) {
+                    resolve({
+                        username: result
+                    })
+                } else {
+                    reject({reason: 'Token passed in header is not valid :-;'})
+                }
+            }))
+            .catch(err => {
+                console.log(err.reason)
+                throw new AuthenticationError(err.reason)
+            })
+        } catch(err) {
+            throw new AuthenticationError(err)
         }
     }
 }
 
-module.exports = contextMiddleware
+module.exports = authMiddleware
